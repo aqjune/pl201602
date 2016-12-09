2f2cb862a36aa282a8bdcb0a6e7985d41b4662f4
Juneyoung Lee <aqjune@gmail.com>
2016-12-09 13:28:46 +0900

    Add mysol 06

--- assignments/06_mysol/P09.v
+++ assignments/06_mysol/P09.v
@@ -11,6 +11,17 @@ Require Export P08.
 
 (** First prove an easy useful lemma. *)
 
+Lemma lt_S: forall n m,
+    S n < S m -> n < m.
+Proof.
+intros.
+induction m.
+- inversion H. inversion H1.
+- inversion H. auto.
+  apply IHm in H1.
+  constructor.
+  assumption.
+Qed.
 
 Lemma in_split : forall (X:Type) (x:X) (l:list X),
   In x l ->
@@ -41,6 +52,30 @@ Inductive repeats {X:Type} : list X -> Prop :=
 | rp_next: forall x l, repeats l -> repeats (x::l)
 .
 
+Theorem len_plus: forall (X:Type) (l1:list X) (l2:list X),
+    length (l1++l2) = length l1 + length l2.
+Proof.
+intros.
+induction l1.
+- reflexivity.
+- simpl. rewrite IHl1. auto.
+Qed.
+
+Theorem in_case_diff:forall (X:Type) (x:X) (x2:X) (l1 l2:list X),
+  In x2 (l1++x::l2) -> x <> x2 -> In x2 (l1++l2).
+Proof.
+intros.
+induction l1.
+- simpl. simpl in H. destruct H.
+  + apply H0 in H. inversion H.
+  + assumption.
+- simpl. simpl in H.
+  destruct H.
+  + auto.
+  + right. auto.
+Qed.
+
+
 (** Now, here's a way to formalize the pigeonhole principle.  Suppose
     list [l2] represents a list of pigeonhole labels, and list [l1]
     represents the labels assigned to a list of items.  If there are
@@ -70,7 +105,25 @@ Proof.
      assert (H' := H_excluded (In x l1')).
      destruct H'.
      - apply rp_base. assumption.
-     - 
+     - apply rp_next.
+       destruct (in_split X x l2).
+       + apply H1.
+         constructor. reflexivity.
+       + inversion H0.
+         apply IHl1' with (l2 := x0++x1).
+         * assumption.
+         * intros.
+           destruct (H_excluded (x = x2)) eqn:Heqxx2.
+           ++ rewrite <- e in H4. apply H in H4. inversion H4.
+           ++ apply in_case_diff with (x := x).
+              rewrite <- H3. apply H1.
+              simpl. right. assumption. assumption.
+         *  rewrite H3 in H2.
+            rewrite len_plus in H2.
+            simpl in H2.
+            rewrite len_plus.
+            rewrite <- plus_n_Sm in H2.
+            apply lt_S. assumption.
    }
 Qed.
 
